967410fd804e4b29299ed2ea44b311d521e1ea05
D1353L <meghanik400@gmail.com>
2015-06-12 09:13:07 +0300

    DB queries are fixed

--- src/main/java/com/chat/server/PostgreSQLJDBC.java
+++ src/main/java/com/chat/server/PostgreSQLJDBC.java
@@ -2,13 +2,15 @@ package com.chat.server;
 
 import java.sql.Connection;
 import java.sql.DriverManager;
-import java.sql.Statement;
+import java.sql.PreparedStatement;
 import java.sql.ResultSet;
+
+import org.postgresql.util.PSQLException;
 
 public class PostgreSQLJDBC {
 	
 	static Connection c = null;
-	static Statement stmt = null;
+	static PreparedStatement stmt = null;
 	
 	   public PostgreSQLJDBC(String DB_URL, String DB_USERNAME, String DB_PASS) {
 	      try {
@@ -16,37 +18,50 @@ public class PostgreSQLJDBC {
 
 	         c = DriverManager.getConnection("jdbc:postgresql://"+DB_URL+"/postgres", DB_USERNAME, DB_PASS);
 	         
-	         
-	         stmt = c.createStatement();
-	         
-	         stmt.executeUpdate("CREATE DATABASE chatdb");
+	         try{
+	        	 stmt = c.prepareStatement("CREATE DATABASE chatdb");
+	        	 stmt.executeUpdate();
+	         }catch(PSQLException e){}
 	         
 	         
 	         c = DriverManager.getConnection("jdbc:postgresql://"+DB_URL+"/chatdb", DB_USERNAME, DB_PASS);
+	         System.out.println("Connected to DB");
 	         
-	         c.setAutoCommit(false);
+	         c.setAutoCommit(true);
 	         
-	         stmt.executeUpdate("CREATE TABLE IF NOT EXISTS users (login text NOT NULL, email text, password text, first_name text, second_name text, \"position\" text, CONSTRAINT \"Plogin\" PRIMARY KEY (login)) WITH (OIDS=FALSE);");
+	         try{
+	        	 stmt = c.prepareStatement("CREATE TABLE users (login text NOT NULL, email text, password text, first_name text, second_name text, \"position\" text, CONSTRAINT \"Plogin\" PRIMARY KEY (login)) WITH (OIDS=FALSE);");
+	        	 stmt.executeUpdate();
+	        	 System.out.println("Table users is created");
+	         }catch(PSQLException e){}
 	         
-	         if(stmt.executeUpdate("CREATE TABLE IF NOT EXISTS messages (sender text NOT NULL, receiver text NOT NULL, message text, id serial NOT NULL, CONSTRAINT \"pId\" PRIMARY KEY (id), CONSTRAINT \"fReceiver\" FOREIGN KEY (receiver) REFERENCES users (login) MATCH SIMPLE ON UPDATE NO ACTION ON DELETE NO ACTION, CONSTRAINT \"fSender\" FOREIGN KEY (sender) REFERENCES users (login) MATCH SIMPLE ON UPDATE NO ACTION ON DELETE NO ACTION) WITH ( OIDS=FALSE);") > 0)
-	        	 stmt.executeUpdate("CREATE INDEX \"fki_fReceiver\" ON messages USING btree (receiver COLLATE pg_catalog.\"default\"); CREATE INDEX \"fki_fSender\" ON messages USING btree (sender COLLATE pg_catalog.\"default\");");
-	      
+	         try{
+	        	 stmt = c.prepareStatement("CREATE TABLE messages (sender text NOT NULL, receiver text NOT NULL, message text, id serial NOT NULL, CONSTRAINT \"pId\" PRIMARY KEY (id), CONSTRAINT \"fReceiver\" FOREIGN KEY (receiver) REFERENCES users (login) MATCH SIMPLE ON UPDATE NO ACTION ON DELETE NO ACTION, CONSTRAINT \"fSender\" FOREIGN KEY (sender) REFERENCES users (login) MATCH SIMPLE ON UPDATE NO ACTION ON DELETE NO ACTION) WITH ( OIDS=FALSE);");
+	        	 stmt.executeUpdate();
+	        	 System.out.println("Table messages is created");
+	         }catch(PSQLException e){}
+	         
+	         try{
+	        	 stmt = c.prepareStatement("CREATE INDEX \"fki_fReceiver\" ON messages USING btree (receiver COLLATE pg_catalog.\"default\"); CREATE INDEX \"fki_fSender\" ON messages USING btree (sender COLLATE pg_catalog.\"default\");");
+	        	 stmt.executeUpdate();
+	        	 System.out.println("Indexes for table messages are created");
+	         }catch(PSQLException e){}
+	         
 	      } catch (Exception e) {
 	         e.printStackTrace();
 	         System.err.println(e.getClass().getName()+": "+e.getMessage());
 	         System.exit(0);
 	      }
-	      System.out.println("Connected to DB");
+	      
 	   }
 	   
 	   public String AddNewUser(String login, String email, String pass, String fName, String sName, String position)
 	   {
 		   try {
-				   stmt = c.createStatement();
 				   String sql = String.format("INSERT INTO users VALUES ('%s', '%s', '%s', '%s', '%s', '%s');", login, email, pass, fName, sName, position);
-				   stmt.executeUpdate(sql);
+				   stmt = c.prepareStatement(sql);
+				   stmt.executeUpdate();
 				   stmt.close();
-				   c.commit();
 				   return "";
 		   }catch(Exception e){System.err.println( e.getClass().getName()+": "+ e.getMessage()); return e.getMessage();}
 	   }
@@ -54,8 +69,8 @@ public class PostgreSQLJDBC {
 	   public boolean IsUserExist(String username)
 	   {
 		   try {
-			   stmt = c.createStatement();
-			   ResultSet rs = stmt.executeQuery( "SELECT login FROM users;" );
+			   stmt = c.prepareStatement("SELECT login FROM users;");
+			   ResultSet rs = stmt.executeQuery();
 			   while (rs.next()){
 				   String  name = rs.getString("login");
 				   if(name.equals(username)){
@@ -71,8 +86,8 @@ public class PostgreSQLJDBC {
 	   public boolean IsEmailExist(String email)
 	   {
 		   try {
-			   stmt = c.createStatement();
-			   ResultSet rs = stmt.executeQuery( "SELECT email FROM users;" );
+			   stmt = c.prepareStatement("SELECT email FROM users;");
+			   ResultSet rs = stmt.executeQuery();
 			   while (rs.next()){
 				   String  em = rs.getString("email");
 				   if(em.equals(email)){
@@ -89,8 +104,8 @@ public class PostgreSQLJDBC {
 	   {
 		   try {
 			   if(IsUserExist(username)){
-				   stmt = c.createStatement();
-				   ResultSet rs = stmt.executeQuery( "SELECT * FROM users WHERE login = '"+username+"';" );
+				   stmt = c.prepareStatement("SELECT * FROM users WHERE login = '"+username+"';");
+				   ResultSet rs = stmt.executeQuery();
 				   
 				   while (rs.next()){
 					   String  name = rs.getString("login");
@@ -113,8 +128,8 @@ public class PostgreSQLJDBC {
 	   public String getEmail(String login)
 	   {
 		   try {
-			   stmt = c.createStatement();
-			   ResultSet rs = stmt.executeQuery( "SELECT * FROM users WHERE login = '"+login+"';" );
+			   stmt = c.prepareStatement("SELECT * FROM users WHERE login = '"+login+"';");
+			   ResultSet rs = stmt.executeQuery();
 		   
 			   while (rs.next()){
 				   String email = rs.getString("email");
@@ -131,8 +146,8 @@ public class PostgreSQLJDBC {
 	   public String getFname(String login)
 	   {
 		   try {
-			   stmt = c.createStatement();
-			   ResultSet rs = stmt.executeQuery( "SELECT * FROM users WHERE login = '"+login+"';" );
+			   stmt = c.prepareStatement("SELECT * FROM users WHERE login = '"+login+"';");
+			   ResultSet rs = stmt.executeQuery();
 		   
 			   while (rs.next()){
 				   String fName = rs.getString("first_name");
@@ -149,8 +164,8 @@ public class PostgreSQLJDBC {
 	   public String getSName(String login)
 	   {
 		   try {
-			   stmt = c.createStatement();
-			   ResultSet rs = stmt.executeQuery( "SELECT * FROM users WHERE login = '"+login+"';" );
+			   stmt = c.prepareStatement("SELECT * FROM users WHERE login = '"+login+"';");
+			   ResultSet rs = stmt.executeQuery();
 		   
 			   while (rs.next()){
 				   String sName = rs.getString("second_name");
@@ -167,8 +182,8 @@ public class PostgreSQLJDBC {
 	   public String getPosition(String login)
 	   {
 		   try {
-			   stmt = c.createStatement();
-			   ResultSet rs = stmt.executeQuery( "SELECT * FROM users WHERE login = '"+login+"';" );
+			   stmt = c.prepareStatement("SELECT * FROM users WHERE login = '"+login+"';");
+			   ResultSet rs = stmt.executeQuery();
 		   
 			   while (rs.next()){
 				   String position = rs.getString("position");
@@ -185,11 +200,10 @@ public class PostgreSQLJDBC {
 	   public String setEmail(String login, String email)
 	   {
 		   try {
-			   stmt = c.createStatement();
 			   String sql = String.format("UPDATE users SET email = '%s' WHERE login = '%s';", email, login);
-			   stmt.executeUpdate(sql);
+			   stmt = c.prepareStatement(sql);
+			   stmt.executeUpdate();
 			   stmt.close();
-			   c.commit();
 			   return "";
 		   }catch(Exception e){System.err.println( e.getClass().getName()+": "+ e.getMessage()); return e.getMessage();}
 	   }
@@ -197,11 +211,10 @@ public class PostgreSQLJDBC {
 	   public String setPassword(String login, String pass)
 	   {
 		   try {
-			   stmt = c.createStatement();
 			   String sql = String.format("UPDATE users SET password = '%s' WHERE login = '%s';", pass, login);
-			   stmt.executeUpdate(sql);
+			   stmt = c.prepareStatement(sql);
+			   stmt.executeUpdate();
 			   stmt.close();
-			   c.commit();
 			   return "";
 		   }catch(Exception e){System.err.println( e.getClass().getName()+": "+ e.getMessage()); return e.getMessage();}
 	   }
@@ -209,11 +222,10 @@ public class PostgreSQLJDBC {
 	   public String setFName(String login, String fName)
 	   {
 		   try {
-			   stmt = c.createStatement();
 			   String sql = String.format("UPDATE users SET first_name = '%s' WHERE login = '%s';", fName, login);
-			   stmt.executeUpdate(sql);
+			   stmt = c.prepareStatement(sql);
+			   stmt.executeUpdate();
 			   stmt.close();
-			   c.commit();
 			   return "";
 		   }catch(Exception e){System.err.println( e.getClass().getName()+": "+ e.getMessage()); return e.getMessage();}
 	   }
@@ -221,11 +233,10 @@ public class PostgreSQLJDBC {
 	   public String setSName(String login, String sName)
 	   {
 		   try {
-			   stmt = c.createStatement();
 			   String sql = String.format("UPDATE users SET second_name = '%s' WHERE login = '%s';", sName, login);
-			   stmt.executeUpdate(sql);
+			   stmt = c.prepareStatement(sql);
+			   stmt.executeUpdate();
 			   stmt.close();
-			   c.commit();
 			   return "";
 		   }catch(Exception e){System.err.println( e.getClass().getName()+": "+ e.getMessage()); return e.getMessage();}
 	   }
@@ -233,11 +244,10 @@ public class PostgreSQLJDBC {
 	   public String setPosition(String login, String pos)
 	   {
 		   try {
-			   stmt = c.createStatement();
 			   String sql = String.format("UPDATE users SET position = '%s' WHERE login = '%s';", pos, login);
-			   stmt.executeUpdate(sql);
+			   stmt = c.prepareStatement(sql);
+			   stmt.executeUpdate();
 			   stmt.close();
-			   c.commit();
 			   return "";
 		   }catch(Exception e){System.err.println( e.getClass().getName()+": "+ e.getMessage()); return e.getMessage();}
 	   }
@@ -245,11 +255,10 @@ public class PostgreSQLJDBC {
 	   public String addMessage(String sender, String receiver, String message)
 	   {
 		   try {
-			   stmt = c.createStatement();
 			   String sql = String.format("INSERT INTO messages VALUES ('%s', '%s', '%s');", sender, receiver, message);
-			   stmt.executeUpdate(sql);
+			   stmt = c.prepareStatement(sql);
+			   stmt.executeUpdate();
 			   stmt.close();
-			   c.commit();
 			   return "";
 		   }catch(Exception e){System.err.println( e.getClass().getName()+": "+ e.getMessage()); return e.getMessage();}
 	   }
@@ -257,8 +266,8 @@ public class PostgreSQLJDBC {
 	   public String getMessages(String user1, String user2)
 	   {
 		   try {
-			   stmt = c.createStatement();
-			   ResultSet rs = stmt.executeQuery( "SELECT message FROM messages WHERE (sender = '"+user1+"' AND receiver = '"+user2+"') OR (sender = '"+user2+"' AND receiver = '"+user1+"');" );
+			   stmt = c.prepareStatement("SELECT message FROM messages WHERE (sender = '"+user1+"' AND receiver = '"+user2+"') OR (sender = '"+user2+"' AND receiver = '"+user1+"');");
+			   ResultSet rs = stmt.executeQuery();
 			   
 			   String msg = "";
 			   while (rs.next()) msg = msg+rs.getString("message");
